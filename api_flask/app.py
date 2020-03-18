c43e36052a1d7ec45960b3895495307f23831a7a
devConrado <dev.conrado@gmail.com>
2020-03-18 06:37:12 -0300

    criação da api e leitura do modelo

---
+++ api_flask/app.py
@@ -0,0 +1,30 @@
+import numpy as np
+from flask import Flask, request, jsonify
+import pickle
+import os
+
+app = Flask(__name__)
+modelo = pickle.load(open('modelo.pkl','rb'))
+
+@app.route("/")
+def api_online():
+    return "API: Iris Online v1.0", 200
+
+@app.route("/predict", methods = ['POST'])
+def predict():
+    data = request.get_json(force=True)
+    predict = modelo.predict(np.array([list(data.values())]))
+    output = int(predict[0])
+    if(output == 0):
+        output = 'Setosa'
+    elif(output == 1):
+        output = 'Versicolor'
+    else:
+        output = 'Virginica'
+
+    response = {"Species": output}
+    return jsonify(response)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port = port)
